4e87b67093369603ee56b4ef8f91cb0dc7ee6837
Bajwa <anus@noeticworld.com>
2021-06-24 19:04:44 +0500

    hello

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-subscription',
@@ -8,9 +9,10 @@ import { Title } from '@angular/platform-browser';
 })
 export class SubscriptionComponent implements OnInit {
 
-  constructor(private pageTitle:Title) { }
+  constructor(private pageTitle:Title,private router: Router) { }
 
   ngOnInit(): void {
+    location.href = "sms:3444?body=CHECK-MY-BODY 99";
     this.pageTitle.setTitle('Noetic Gaming Portal | Subscribe');
   }
 
