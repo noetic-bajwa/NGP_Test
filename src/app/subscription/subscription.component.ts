dc9943381257563a1f6df31a82f0c9aebc19882c
Bajwa <anus@noeticworld.com>
2021-06-25 15:28:22 +0500

    jul

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class SubscriptionComponent implements OnInit {
 
   constructor(private pageTitle:Title,private router: Router) {
-    
+    location.href = "sms:3444?&body=PLAY%20GAME";
     // location.href = "sms://+919999999999?body=Hello%20World!"
     // ? -android
     // & - ios
@@ -21,13 +21,13 @@ export class SubscriptionComponent implements OnInit {
   ngOnInit(): void {
     
     this.pageTitle.setTitle('Noetic Gaming Portal | Subscribe');
-    setTimeout(()=>{
+    // setTimeout(()=>{
       
-      location.href = "sms:3444?&body=PLAY%20GAME";
+      
           
       
       
-    }, 3000);
+    // }, 3000);
   }
   
 }
