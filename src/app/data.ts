40e61b10bf472017772f47e0fdb020cecb651c64
Bajwa <anus@noeticworld.com>
2021-06-29 18:06:36 +0500

    papu

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,47 +1,127 @@
 export const GAMES:any = [
     {
-        id:0,
-        title:"Temple Run 2",
+        id:1,
+        title:"Winter Soccer",
         tag:"new",
-        description:"Play Temple Run 2 Online - Run, jump, turn and slide through the endless landscape once again in this online version of the game Temple Run 2!",
-        instructions:"Mouse control and touch control on mobile",
-        category:['Action','Arcade'],
-        imgURL:"/assets/Games Assets/Temple Run 2/images/2.jpg",
-        iframeSrc:"https://html5.gamemonetize.com/pkyyuilfrqkcdnmrxsg60j22ypk0peje/",
+        description:"Winter season is here, but the soccer game must continue. Drag, aim and release to shoot the soccer ball. The first person to score 3 goals wins. Features: - Winter theme with snow and pine trees, perfect for the winter / christmas audiance - Choose your side, from 32 nations. - Smart and calculative AI. Be prepared to think, plan and play hard - Tutorial to learn the ropes - Exciting soccer stadium environment, perfect for soccer or football nuts",
+        instructions:"Play soccer in the winter",
+        category:['Sports'],
+        imgURL:"/assets/Games Assets/Winter Soccer/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=4797390457405440&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200",
         rating:4
     },
     {
-        id:1,
-        title:"Hunter Assassin",
+        id:2,
+        title:"Fast Lane Racing",
         tag:"new",
-        description:"Control the assassin and hunt down your targets one by one. Use your surroundings and shadows to stay hidden from flashlights. Targets with automatic machine guns can be lurking around each corner. Attack them swiftly and escape without being spotted.",
-        instructions:"Mouse control and touch control on mobile",
-        category:['Action','Arcade','Adventure'],
-        imgURL:"/assets/Games Assets/Hunter Assassin/images/1.jpg",
+        description:"Race against other cars. Tap to switch lanes. Don't crash into other cars! Keep on your toes and be prepared to switch lanes quickly in this fastlane game. Features: - Endless gameplay - Urban hip hop racing theme - Extremely simple controls - Simple, linear high score system Fans of racing and driving games will love this.",
+        instructions:"Switch lanes and move fast!",
+        category:['Action','Endless'],
+        imgURL:"/assets/Games Assets/Fast Lane Racing/1.jpg",
         iframeSrc:"https://html5.gamemonetize.com/l1c9pkpxysqy1cao2axakpf8mdwkv8qg/" ,
         rating:5
     },
     {
-        id:2,
-        title:"Candy Crush Saga",
+        id:3,
+        title:"Ski Slopes",
         tag:"new",
-        description:"Plan your moves by matching 3 or more candies in a row, using boosters wisely in order to overcome those extra sticky puzzles! Blast the chocolate and collect sweet candy across thousands of levels, guaranteed to have you craving more!",
-        instructions:"Start playing Candy Crush Saga today by moving sweet candies.",
-        category:['Hypercasual','Puzzle'],
-        imgURL:"/assets/Games Assets/Candy Crush Saga King/images/2.jpg",
-        iframeSrc:"https://html5.gamemonetize.com/ushitaltif0ig5by3w54p4cy9sd3yptt/",
+        description:"Ski down the mountain slopes. Tap to jump over obstacles. Perform cool backflips and frontflips in mid air. Collect coins to upgrade your snowman. Features: - Beautiful winter theme - Endless gameplay - Use coins to purchase upgrades in the store - Score points to unlock special snowman characters. Perfect for holidays, winter and seasonal campaigns such as Christmas.",
+        instructions:"Navigate mountain slopes and perform cool stunts",
+        category:['Adventure','Sports'],
+        imgURL:"/assets/Games Assets/Ski Slopes/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=4878847326552064&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200",
         rating:5 
     },
     {
-        id:3,
-        title:"Fast Lane Racing",
+        id:4,
+        title:"Cookie Line",
         tag:"new",
-        description:"Plan your moves by matching 3 or more candies in a row, using boosters wisely in order to overcome those extra sticky puzzles! Blast the chocolate and collect sweet candy across thousands of levels, guaranteed to have you craving more!",
-        instructions:"Start playing Candy Crush Saga today by moving sweet candies.",
-        category:['Hypercasual','Puzzle'],
+        description:"Drag and drop the hexagon pieces onto empty slots. Create full lines to eliminate the tiles. What's your highest score? Features: - Fun and cute theme, to induce focus - Score combo points by clearing 2 or more lines simultaneously",
+        instructions:"Complete the lines in this hexagon cookie puzzle",
+        category:['Puzzle'],
         imgURL:"/assets/Games Assets/Fast Lane Racing/1.jpg",
-        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=6496463023505408&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=6510494950096896&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
     }
-
-
+    ,
+    {
+        id:5,
+        title:"Sort the trash",
+        tag:"popular",
+        description:"Protect the environment by learning how to recycle correctly. Collect items that can be recycled only. Avoid collecting contaminators such as organic waste, disposable coffee cups, plastic straws, ceramic dishware and lithium ion batteries. Go green and save the Earth. Start becoming eco-friendly today! Features: - Simple elegant design - Responsive design - Positive them with recycling facts",
+        instructions:"Collect recyclable items, avoid contaminators",
+        category:['Family','Memory','Educational'],
+        imgURL:"/assets/Games Assets/Sort The Trash/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=6317311196659712&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    },
+    {
+        id:6,
+        title:"Super Bubble Shooter",
+        tag:"popular",
+        description:"Pop 3 or more bubbles of the same color. Navigate around obstacles such as rock bubbles, glass bubbles and spikes. How many stars can you score for each level? Features - Use a variety of power ups such as bombs Rainbow bubbles, Thunder Strikes and Shields to achieve your objectives - Over 60 challenging levels in this classic bubble shooter game",
+        instructions:"Match bubbles of the same color",
+        category:['Family','Puzzle'],
+        imgURL:"/assets/Games Assets/Super Bubble Shooter/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=6693820998615040&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    },
+    {
+        id:7,
+        title:"Sumo Push Push ",
+        tag:"popular",
+        description:"Tap on the dirt lanes, to launch your Sumo heroes forward. Overpower your opponents with clever placement of Sumo warriors. Make use of different Sumo weight classes to win lane control, in this fast paced multiplayer PvP game. Features: - Exciting Player vs Player (PvP) game mechanic - Sumo theme with Sumo warriors and hero theme - Random selection of opponents in this multiplayer game - Play with your friends in real-time - Taunt your friends using emoticons - Chat with your friends while playing",
+        instructions:"Attack and defend lanes with Sumo warriors",
+        category:['Action','Multiplayer'],
+        imgURL:"/assets/Games Assets/Sumo/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=5386651997044736&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    },
+    {
+        id:8,
+        title:"Kawani Jump ",
+        tag:"popular",
+        description:" In Kawaii Jump, control a cute little purple slime that jumps to the top. Hop your way on to solid tiles. Avoid dangerous tiles. Avoid tiles that will break, at all costs! How high can you go? Features: - Unlock kawaii characters such a Cow, Bear, Buffalo, Whale, Unicorn, Shark, Goat, Dino and more - Collect powerups such as extra time, combo points, Trampoline, Safe Zone and more! - Beautiful theme - Addictive music that's hard to forget - Easy to play, hard to master",
+        instructions:"Jump above, jump fast, jump high!",
+        category:['Endless','Platformer'],
+        imgURL:"/assets/Games Assets/Kawaii Jump/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=5070286719287296&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    },
+    {
+        id:9,
+        title:"Dodgeball Battle",
+        tag:"like",
+        description:" Catch the ball with your hands, and throw it back to them. Can you win this duel dodge ball game? Use your reflexes and wits to defeat the opponent. Features: - Power mode - Endless gameplay - Unlock new skins - Responsive design - Clever AI",
+        instructions:"Throw the ball at your opponent. Don't get hit!",
+        category:['Action','Sports'],
+        imgURL:"/assets/Games Assets/DodgeBall Battle/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=5390016617381888&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    },
+    {
+        id:10,
+        title:"Bill The Bowman ",
+        tag:"like",
+        description:"Fancy being Robinhood, the expert archer? Now you can practise your archery skills. Tap and aim your bow. Release to shoot the arrow. Aim for the fruit, not the person. Encounter an increasingly challenging landscape that will challenge your aiming skills. How high can you score? Features: - 2x combo when you hit a winning stream - Hit 5 fruits in a row to get an extra arrow - Fun medieval theme - Endless gameplay This game is perfect for robin hood lovers who like all things medieval including knights in shining armor, merlin wizards, monks and yes, fire.",
+        instructions:"Aim and shoot arrows at fruits",
+        category:['Action','Sports'],
+        imgURL:"/assets/Games Assets/Bill The Bowman/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=4919276580372480&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    },
+    {
+        id:11,
+        title:"Zero Collision ",
+        tag:"like",
+        description:"Race your car around the tracks, but beware of the opposing cars. Oh, don't forget to avoid the other obstacles such as oil slicks and frost! How many points can you score? Features: Easy to start, hard to master Increasingly smarter enemy cars. It gets harder and harder to avoid them  Unlimited gameplay. Literally spend hours trying to compete with yourself! Different race environments, ranging from grass, desert to water Fun vibe and music",
+        instructions:"Race while avoiding the blue cars",
+        category:['Action','Sports'],
+        imgURL:"/assets/Games Assets/Zero Collision/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=5703040130613248&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    },
+    {
+        id:12   ,
+        title:"City Ball Dunkin",
+        tag:"like",
+        description:" Dunk the basketball into each hoop. Time your dunks well. Make sure you don't hit the ceiling, or fall to the group. Collect points to unlock different cool balls from the store. Beware, the challenges get more difficult over time! Make your way past tricky hoops that move in weird ways. How high can you score? Features: - A store to unlock different balls. Really cool ones. - Fun theme - Day and night transition - Increasingly challenging gameplay",
+        instructions:"Put the basketball through the hoop",
+        category:['Sports','Adventure'],
+        imgURL:"/assets/Games Assets/City Ball Dunkin/1.jpg",
+        iframeSrc:"https://subscription.marketjs.com/serve?portal_id=5692266857365504&game_id=5070286719287296&branding_id=5169425925799936&ad_id=5185095820378112&portal_localization_id=6207741047603200" 
+    }
+ 
 ]
