547e771e298d38474f6f3572eac98f4ead19d11b
Bajwa <anus@noeticworld.com>
2021-06-30 16:10:55 +0500

    hen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { AuthService } from 'src/app/auth.service';
 import { NgForm } from '@angular/forms';
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { bounceInDown } from 'ng-animate';
 declare let AOS: any;
 
 // import { OwlOptions } from 'ngx-owl-carousel-o';
@@ -11,11 +13,25 @@ declare let AOS: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'] ,
+  animations: [
+    trigger('bounceInDown', [transition('* => *', useAnimation(bounceInDown, {
+      params: {
+        timing: 1,
+    
+        // Specify granular values for `translate` on axis Y during 'bounceInDown' 
+        a: '-3000px',
+        b: '25px',
+        c: '-10px',
+        d: '5px',
+      }
+    }))])
+  ]
 })
 
 
 export class LoginComponent implements OnInit {
+  bounceInDown: any;
   number = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
   isActive=5; 
   name:number;
--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,17 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { bounceInDown } from 'ng-animate';
+import { ActivatedRoute } from '@angular/router';
 declare let AOS: any;
 
 @Component({
   selector: 'app-subscription',
   templateUrl: './subscription.component.html',
-  styleUrls: ['./subscription.component.css']
+  styleUrls: ['./subscription.component.css'] ,
+  animations: [
+    trigger('bounceInDown', [transition('* => *', useAnimation(bounceInDown, {
+      params: {
+        timing: 1,
+    
+        // Specify granular values for `translate` on axis Y during 'bounceInDown' 
+        a: '-3000px',
+        b: '25px',
+        c: '-10px',
+        d: '5px',
+      }
+    }))])
+  ]
 })
 export class SubscriptionComponent implements OnInit {
+  partner : string;
+  trackerId : string;
+
+  bounceInDown: any;
   number = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
   isActive=5; 
-  constructor(private pageTitle:Title,private router: Router) {
+  orderby: string;
+  constructor(private pageTitle:Title,private router: Router,private route: ActivatedRoute) {
+    this.router.navigate(['/subscribe'], { queryParams: { 'partner': 'ttc', 'trackerId': '123' } });
     
     // location.href = "sms:3444?&body=PLAY%20GAME";
     // location.href = "sms://+919999999999?body=Hello%20World!"
@@ -20,14 +42,23 @@ export class SubscriptionComponent implements OnInit {
     
     
    }
-
+   
   ngOnInit(): void {
+    
     AOS.init();
     this.pageTitle.setTitle('Noetic Gaming Portal | Play');
     // setTimeout(()=>{
       
       
-          
+      this.route.queryParams
+      .subscribe(params => {
+        console.log(params); // { orderby: "price" }
+        this.partner = (params.partner)
+        this.trackerId = (params.trackerId)
+        // this.orderby = params.orderby;
+        // console.log(this.orderby); // price
+      }
+    );     
       
       
     // }, 3000);
